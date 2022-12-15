edc9943a36fa14d0491e9b3edb28e45057179111
dontech09 <donnjondanmakal@gmail.com>
2022-12-15 21:57:37 +0530

    Updated Dashboard

--- components/addUser/adduser.js
+++ components/addUser/adduser.js
@@ -1,6 +1,49 @@
+import axios from "axios";
+import { useState } from "react";
+import Loader from "../loader/loader";
+
 export default function Adduser() {
+  const [user, setUser] = useState({
+    consumer_id: "",
+    username: "",
+    address: "",
+    phone: "",
+    password: "",
+  });
+  const [error, setError] = useState("");
+  const [uploading, setuploading] = useState(false);
+  const createUser = () => {
+    if (user.consumer_id.length < 5) {
+      setError("Enter a valid consumer id");
+    } else if (user.username.length < 5) {
+      setError("Enter a valid username");
+    } else if (user.password.length < 5) {
+      setError("Minimum password length 5");
+    } else if (user.consumer_id.length < 5) {
+      setError("Enter a valid consumer id");
+    } else {
+      setError("");
+      setuploading(true);
+      console.log(user);
+    }
+    axios({
+      method: "post",
+      url: "https://dbms-api.vercel.app/users",
+      data: user,
+    })
+      .then((e) => {
+        console.log(e);
+        setuploading(false);
+        setError("User added succesfully 🤩");
+      })
+      .catch((e) => {
+        console.log(e);
+        setError("Something went wrong!");
+      });
+  };
   return (
     <div className="p-16 ">
+      {uploading ? <Loader text="Adding new user" /> : ""}
       <h2 className="font-bold text-2xl">Add User </h2>
       <div className="line w-full mt-3"></div>
       <div className="p-6">
@@ -9,6 +52,9 @@ export default function Adduser() {
             <div className="">
               <p className="text text-lg font-medium">Username</p>
               <input
+                onChange={(e) => {
+                  setUser({ ...user, username: e.target.value });
+                }}
                 type="text"
                 className="input-box mt-1 w-4/6 outline-none p-2 rounded"
                 placeholder="User name"
@@ -17,6 +63,9 @@ export default function Adduser() {
             <div className="">
               <p className="text text-lg font-medium">Password</p>
               <input
+                onChange={(e) => {
+                  setUser({ ...user, password: e.target.value });
+                }}
                 type="text"
                 className="input-box mt-1 w-4/6 outline-none p-2 rounded"
                 placeholder="password"
@@ -25,17 +74,52 @@ export default function Adduser() {
             <div className="">
               <p className="text text-lg font-medium">Consumer Number</p>
               <input
+                onChange={(e) => {
+                  setUser({ ...user, consumer_id: e.target.value });
+                }}
                 type="text"
                 className="input-box mt-1 w-4/6 outline-none p-2 rounded"
                 placeholder="Consumer Number"
               />
             </div>{" "}
-            <div className=""></div>
             <div className="">
-              <button className="text-sm hover:opacity-80 transition-all rounded-sm  w-40 p-2 text-white font-medium bg-blue-400 dark-blue  mt-4">
+              <p className="text text-lg font-medium">Address</p>
+              <input
+                onChange={(e) => {
+                  setUser({ ...user, address: e.target.value });
+                }}
+                type="text"
+                className="input-box mt-1 w-4/6 outline-none p-2 rounded"
+                placeholder="Consumer Address"
+              />
+            </div>{" "}
+            <div className="">
+              <p className="text text-lg font-medium">Phone Number</p>
+              <input
+                type="tel"
+                onChange={(e) => {
+                  setUser({ ...user, phone: e.target.value });
+                }}
+                className="input-box mt-1 w-4/6 outline-none p-2 rounded"
+                placeholder="Consumer Phone Number"
+              />
+            </div>
+            <div className=""></div>
+            {error ? (
+              <div className=" col-span-2">
+                <p className="text-red-500">{error}</p>
+              </div>
+            ) : (
+              ""
+            )}
+            <div className="">
+              <button
+                onClick={createUser}
+                className="text-sm hover:opacity-80 transition-all rounded-sm  w-40 p-2 text-white font-medium bg-blue-400 dark-blue  mt-1"
+              >
                 Add User
               </button>
-              <button className="text-sm button-off ml-4 hover:opacity-80 transition-all rounded-sm  w-40 p-2 text-white font-medium bg-blue-400 dark-blue  mt-4">
+              <button className="text-sm button-off ml-4 hover:opacity-80 transition-all rounded-sm  w-40 p-2 text-white font-medium bg-blue-400 dark-blue  mt-1">
                 View All Users
               </button>
             </div>
--- components/bill/bill.js
+++ components/bill/bill.js
@@ -1,4 +1,46 @@
+import axios from "axios";
+import { useState } from "react";
+import { IoIosCheckmarkCircle } from "react-icons/io";
 export default function Bill() {
+  const [consumerid, setConsumerid] = useState();
+  const [verified, setVerified] = useState();
+  const [username, setUsername] = useState("");
+  const [data, setData] = useState({
+    consumer_id: "",
+    units: "",
+    amount: "",
+    due_date: "",
+  });
+  const verify = () => {
+    if (consumerid == "") {
+      return;
+    }
+    axios({
+      method: "get",
+      url: `https://dbms-api.vercel.app/users/${consumerid}`,
+      responseType: "json",
+    }).then(function (response) {
+      console.log(response.data);
+      let data = response.data;
+      let verified = false;
+      setVerified(false);
+      for (let i = 0; i < data.length; i++) {
+        console.log(
+          data[i].consumer_id,
+          consumerid,
+          data[i].consumer_id == consumerid
+        );
+        if (data[i].consumer_id == consumerid) {
+          verified = true;
+          setVerified(true);
+          setUsername(data[i].username);
+          break;
+        }
+      }
+      console.log(verified);
+    });
+  };
+  const submitBill = () => {};
   return (
     <div className="p-16 ">
       <h2 className="font-bold text-2xl">Add User Bills</h2>
@@ -6,51 +48,82 @@ export default function Bill() {
       <div className="p-6">
         <div className="flex flex-col">
           <p className="text text-lg font-medium">Consumer Number</p>
-          <input
-            type="text"
-            className="input-box mt-1 w-2/6 outline-none p-2 rounded"
-            placeholder="Consumer Number"
-          />
-          <button className="text-sm hover:opacity-80 transition-all rounded-sm  w-40 p-2 text-white font-medium bg-blue-400 dark-blue  mt-4">
+          <div className="flex flex-row items-center">
+            <input
+              onChange={(e) => setConsumerid(e.target.value)}
+              type="text"
+              className="input-box mr-4 mt-1 w-2/6 outline-none p-2 rounded"
+              placeholder="Consumer Number"
+            />
+            {verified ? (
+              <IoIosCheckmarkCircle size={20} color="#00c853" />
+            ) : (
+              <IoIosCheckmarkCircle size={20} color="#b2ebf2" />
+            )}
+          </div>
+
+          <button
+            onClick={verify}
+            className="text-sm hover:opacity-80 transition-all rounded-sm  w-40 p-2 text-white font-medium bg-blue-400 dark-blue  mt-4"
+          >
             Get User
           </button>
-          <div className="mt-10 grid gap-y-6 grid-cols-2">
-            <div className="">
-              <p className="text text-lg font-medium">Current Reading</p>
-              <input
-                type="text"
-                className="input-box mt-1 w-4/6 outline-none p-2 rounded"
-                placeholder="Current Reading"
-              />
-            </div>{" "}
-            <div className="">
-              <p className="text text-lg font-medium">Unit charge</p>
-              <input
-                type="text"
-                className="input-box mt-1 w-4/6 outline-none p-2 rounded"
-                placeholder="Per unit price"
-              />
-            </div>{" "}
-            <div className="">
-              <p className="text text-lg font-medium">Tax</p>
-              <input
-                type="text"
-                className="input-box mt-1 w-4/6 outline-none p-2 rounded"
-                placeholder="Tax amount"
-              />
-            </div>{" "}
-            <div className="">
-              <p className="text text-lg font-medium">Expiry Date</p>
-              <input
-                type="date"
-                className="input-box mt-1 w-4/6 outline-none p-2 rounded"
-                placeholder="Current Reading"
-              />
-            </div>{" "}
-            <button className="text-sm hover:opacity-80 transition-all rounded-sm  w-40 p-2 text-white font-medium bg-blue-400 dark-blue  mt-4">
-              Generate Bill
-            </button>
-          </div>
+          {verified ? (
+            <div className="mt-10 grid gap-y-6 grid-cols-2">
+              {" "}
+              <div className="">
+                <p className="text text-lg font-medium">Username</p>
+                <input
+                  value={username}
+                  disabled
+                  type="text"
+                  className="input-box mt-1 w-4/6 outline-none p-2 rounded"
+                  placeholder="Current Reading"
+                />
+              </div>{" "}
+              <div className="">
+                <p className="text text-lg font-medium">Current Reading</p>
+                <input
+                  onChange={(e) => setData({ ...data, units: e.target.value })}
+                  type="text"
+                  className="input-box mt-1 w-4/6 outline-none p-2 rounded"
+                  placeholder="Current Reading"
+                />
+              </div>{" "}
+              <div className="">
+                <p className="text text-lg font-medium">Unit charge</p>
+                <input
+                  onChange={(e) => setData({ ...data, units: e.target.value })}
+                  type="text"
+                  className="input-box mt-1 w-4/6 outline-none p-2 rounded"
+                  placeholder="Per unit price"
+                />
+              </div>{" "}
+              <div className="">
+                <p className="text text-lg font-medium">Tax</p>
+                <input
+                  onChange={(e) => setData({ ...data, units: e.target.value })}
+                  type="text"
+                  className="input-box mt-1 w-4/6 outline-none p-2 rounded"
+                  placeholder="Tax amount"
+                />
+              </div>{" "}
+              <div className="">
+                <p className="text text-lg font-medium">Expiry Date</p>
+                <input
+                  type="date"
+                  className="input-box mt-1 w-4/6 outline-none p-2 rounded"
+                  placeholder="Current Reading"
+                />
+              </div>{" "}
+              <div className=""></div>
+              <button className="text-sm hover:opacity-80 transition-all rounded-sm  w-40 p-2 text-white font-medium bg-blue-400 dark-blue  mt-4">
+                Generate Bill
+              </button>
+            </div>
+          ) : (
+            ""
+          )}
         </div>
       </div>
     </div>
